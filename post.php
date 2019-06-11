2f13e1087c246ac54f8cc0a5f4eb8f423c794513
unknown <aleksadoderovic@gmail.com>
2019-06-11 18:09:12 +0200

    add post page

---
+++ post.php
@@ -0,0 +1,32 @@
+<?php include('header.php') ?>
+<main class="main-padding main-margins">
+    <div>
+        <h1>Naslov</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam porro itaque reprehenderit consequuntur officiis. Quae sapiente quibusdam provident praesentium tempora ratione tenetur voluptatum quasi, necessitatibus laborum, placeat voluptas reprehenderit magni itaque ducimus optio, quia est. Hic dolorum alias unde iusto quas, laboriosam at doloremque magni qui porro doloribus facilis mollitia corrupti minima eligendi totam! Iure nostrum numquam recusandae quo obcaecati dolores possimus enim natus, aliquam corporis. Odio magni dolor minus, ullam asperiores voluptatibus odit obcaecati molestiae! Quam culpa repellat repudiandae obcaecati architecto eos natus et ipsa esse saepe quasi pariatur soluta necessitatibus, voluptatum maxime alias odit maiores dicta debitis cupiditate?</p>
+    </div>
+    <div>
+        <h1>Komentari</h1>
+        <div>
+            <div>
+                <h3>Anton 30.01.2019</h3>
+                <p>Meni je ovo bas super :)</p>
+            </div>
+            
+            <div>
+                <h3>Milos 01.02.2019</h3>
+                <p>Pozdrav :)</p>
+            </div>
+            
+            <div>
+                <h3>Ana 05.06.2019</h3>
+                <p>Kad je polaganje. Hvala.</p>
+            </div>
+            
+        </div>
+        <textarea placeholder="Ostavi komentar ovdje..." name="comment" id="comment" cols="60" rows="5"></textarea>
+        <br>
+        <a href="#" class="read-more">Ostavi komentar</a>
+    </div>
+    
+</main>
+<?php include('footer.php') ?>
