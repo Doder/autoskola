fd7382d884b1e6e4229ae3e30e0ffb97d061c3b2
kristinap95 <51536462+kristinap95@users.noreply.github.com>
2019-06-11 13:33:22 +0200

    Update about.php

--- about.php
+++ about.php
@@ -3,7 +3,7 @@
     <img class="about-img" src="img/skola.png" alt="skola">
     <div class="about">
         <h1>O nama</h1>
-        <h2>Auto skola zeleno svijetlo</h2>
+        <h2>Auto skola Zeleni signal</h2>
         <p>Auto škola je osnovana 2004. godine i od tada našim polaznicima dajemo kvalitetnu obuku sa željom i ciljem da postanu dobri i sigurni vozači. Danas je u našoj auto školi je zaposleno šest instruktora koji svojim predanim radom stvaraju dobru atmosferu za osposobljavanje polaznika.</p>
         <p>Dugogodišnje iskustvo, stručni instruktori i predavači te kvalitetan i moderan vozni park ključ su kvalitetnog i stručnog osposobljavanja polaznika. U našem radu uvijek smo u korak sa vremenom, novim dostignućima i standardima u osposobljavanju i sigurnoj vožnji.</p>
         <p>Osposobljavanje se obavlja u svim vremenskim uslovima i saobracajnim prilikama što kandidatu daje svestranost, snalažnjivost i sigurnost u vožnji. Sati održavanja vožnje su koliko god je to moguće prilagođeni slobodnom vremenu kandidata.</p>
