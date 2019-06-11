ec380f95029c7ede7c785a4b6d411ede20d802d7
kristinap95 <51536462+kristinap95@users.noreply.github.com>
2019-06-11 13:35:20 +0200

    Add files via upload

--- login.php
+++ login.php
@@ -3,19 +3,19 @@
 include('pre-body.php');
 ?>
 <main class="login-main">
-    <div class="login-box">
+    <div class="login-box" style = "color:black">
         <form action="">
             <label for="username">Korisnicko ime:</label>
             <br>
             <input type="text" id="username" placeholder="Unesite korisnicko ime...">
             <br>
-            <label for="password">Pasvord:</label>
+            <label for="password">Lozinka:</label>
             <br>
-            <input type="password" id="password" placeholder="Unesite pasvord...">
+            <input type="password" id="password" placeholder="Unesite lozinku...">
             <br>
             <input type="submit" value="Uloguj se">
         </form>
-        <p class="info-small">Nemate nalog, <a href="register.php">napravite ga</a></p>
+        <p class="info-small">Nemate nalog? <a href="register.php">Registruj se</a></p>
     </div>
     
 </main>
--- register.php
+++ register.php
@@ -3,23 +3,23 @@
 include('pre-body.php');
 ?>
 <main class="login-main">
-    <div class="login-box">
+    <div class="login-box" style = "color:black">
         <form action="">
             <label for="username">Korisnicko ime:</label>
             <br>
             <input type="text" id="username" placeholder="Unesite korisnicko ime...">
             <br>
-            <label for="password">Pasvord:</label>
+            <label for="password">Lozinka:</label>
             <br>
-            <input type="password" id="password" placeholder="Unesite pasvord...">
+            <input type="password" id="password" placeholder="Unesite lozinku...">
             <br>
-            <label for="password2">Ponovi pasvord:</label>
+            <label for="password2">Ponovi lozinku:</label>
             <br>
-            <input type="password" id="password2" placeholder="Ponovi pasvord...">
+            <input type="password" id="password2" placeholder="Ponovite lozinku...">
             <br>
             <input type="submit" value="Registruj se">
         </form>
-        <p class="info-small">Imate nalog, <a href="login.php">uloguj se</a></p>
+        <p class="info-small">Imate nalog? <a href="login.php">Uloguj se</a></p>
     </div>
 </main>
 <?php
