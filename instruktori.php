24bf2a41ed398256800ee6f7a0512e24cf9d8b91
kristinap95 <51536462+kristinap95@users.noreply.github.com>
2019-06-15 11:57:10 +0200

    Add files via upload

--- instruktori.php
+++ instruktori.php
@@ -1,9 +1,40 @@
 <?php
-include('pre-body.php');
+include('header.php');
 ?>
+<main class="main-padding main-margins">
+<?php
 
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db = "it_projekat";
+// Create connection
+$conn = new mysqli($servername, $username, $password,$db);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+$sql1 = "SELECT instruktor.id, instruktor.ime, instruktor.prezime, instruktor.slika, kategorijainstruktor.naziv_kategorije
+FROM instruktor
+INNER JOIN kategorijainstruktor ON kategorijainstruktor.id_instruktora = instruktor.id";
+$upit = $conn->query($sql1);
+echo "<table>";
+while($rows = $upit->fetch_assoc())
+{ 
+    echo "<tr>";
+    echo "<td><img src = ".$rows['slika']. " wigth = '100px' height = '120px'></td>";
+    echo "<td>" . $rows['ime'] . " " . $rows['prezime'] . " " . $rows['naziv_kategorije'] . "</td>";
+    echo "</tr>";
+}
+
+echo "<table>";
+$conn->close();
+
+?>
+</main>
 
 
 <?php
-include('post-body.php');
+include('footer.php');
 ?>
